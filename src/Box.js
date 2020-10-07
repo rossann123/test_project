4a9ad035eed4861c3ab97ab5d06d1bbaa61e0caa
rossann123 <54640506+rossann123@users.noreply.github.com>
2020-10-07 17:21:29 -0400

    commit

--- src/Box.js
+++ src/Box.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
+
 function Box(props){
 
-    const [display] = React.useState(props.display);
+    const [name]=React.useState(props.name);
+    const [category]=React.useState(props.category);
+    const [value]=React.useState(props.value);
+    const [img]=React.useState(props.img);
 
-    return (
-        <div>
-            <h1>{display}</h1>
+    return(
+        <div class={"container"}>
+            <div class={"item"}>
+                <h1>Name: {name}</h1>
+                <h2>Category: {category}</h2>
+                <h3>Value: {value}</h3>
+                <img src={img} alt="Funko"/>
+            </div>
         </div>
     )
-
-
 }
 
 export default Box;
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,14 +1,77 @@
-import React from "react";
+import React from 'react';
+const data = [
+    {
+        name:"Goku",
+        category:"animation",
+        value:1,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/33_SuperSaiyanGoku.jpg"
+    },
+    {
+        name:"Vegeta",
+        category:"animation",
+        value:2,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/8151_supersaiyanvegeta_1472654695.jpg"
+    },
+    {
+        name:"Vegito",
+        category:"animation",
+        value:5,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/16554_ssgssvegitometallic_1545253889.jpeg"
+    },
+    {
+        name:"Deku",
+        category:"animation",
+        value:2,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/10498_deku_1495199866.jpg"
+    },
+    {
+        name:"Kazuya",
+        category:"games",
+        value:3,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/9703_kazuya_1487027906.jpg"
+    },
+    {
+        name:"Cortex",
+        category:"games",
+        value:2,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/11825_drneocortex_1505810756.jpg"
+    },
+    {
+        name:"Sonic",
+        category:"games",
+        value:4,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/12315_sonicemerald_1534031350.jpg"
+    },
+    {
+        name:"McCree",
+        category:"games",
+        value:3,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/9789_mccree_1488423232.jpg"
+    },
+    {
+        name:"Michael Scott",
+        category:"television",
+        value:3,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/18115_michaelscott_1559249016.jpg"
+    },
+    {
+        name:"Dwight",
+        category:"television",
+        value:1,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/18118_dwightschrute_1559249425.jpg"
+    },
+    {
+        name:"Jim",
+        category:"television",
+        value:2,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/18116_jimhalpert_1559249178.jpg"
+    },
+    {
+        name:"Pam",
+        category:"television",
+        value:1,
+        img: "https://cdn.poppriceguide.com/guide/itmimg/thumb/18119_pambeesly_1559249505.jpg"
+    }
+]
 
-function Data(props){
-    return(
-        <div style={{backgroundColor: props.match.params.id4}}>
-            <h1>Object ID: {props.match.params.id1}</h1>
-            <h2>Name: {props.match.params.id2}</h2>
-            <h2>Age: {props.match.params.id3}</h2>
-            <h2>Color: {props.match.params.id4}</h2>
-        </div>
-    );
-}
-
-export default Data;
+export default data;
